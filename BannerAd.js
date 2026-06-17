5715f8bc0182ea07cac0c75b5a230c7393c77adc
Sean Moriarty <smoriarty81@gmail.com>
2026-06-16 23:13:13 -0400

    Update BannerAd.js

--- BannerAd.js
+++ BannerAd.js
@@ -18,15 +18,15 @@
       'justify-content:center',
       'width:100%',
       'box-sizing:border-box',
-      'background-color:#FABB6E',
-      'border-color:#F9A15A'
+      'background-color:#e57027',
+      'border-color:#c45e1e'
     ].join(';');
 
     const link = document.createElement('a');
     link.href = 'YOUR_URL_HERE';
     link.target = '_blank';
     link.rel = 'noopener noreferrer';
-    link.style.cssText = 'color:#7a4000; font-size:14px; font-weight:bold; text-decoration:underline;';
+    link.style.cssText = 'color:#fff; font-size:14px; font-weight:bold; text-decoration:underline;';
     link.textContent = 'Download Clarity Go';
 
     newBanner.appendChild(link);
